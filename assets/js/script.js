9f752cd86ef49885c7eb77409e750645b2395c30
Masum Raza <56122183+masumrazait@users.noreply.github.com>
2023-10-09 02:22:44 +0530

    changes in code

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Masum Raza";
+            document.title = "Masum Raza | Portfolio";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
